2afe5fc712f92e9e40288093c1942b8f711a7cc6
Mirko Lenz <mirko@mirkolenz.com>
2022-12-18 00:17:26 +0100

    fix: changelog file not specified

--- release.config.js
+++ release.config.js
@@ -1,57 +1,57 @@
 const config = {
-	// https://semantic-release.gitbook.io/semantic-release/usage/workflow-configuration
-	branches: [
-		{ name: "main" },
-		{ name: "next" },
-		{ name: "+([0-9])?(.{+([0-9]),x}).x" },
-		{ name: "dev", prerelease: true },
-		{ name: "beta", prerelease: true },
-		{ name: "alpha", prerelease: true },
-	],
-	plugins: [
-		[
-			"@semantic-release/commit-analyzer",
-			{
-				preset: "conventionalcommits",
-			},
-		],
-		[
-			"@semantic-release/release-notes-generator",
-			{
-				preset: "conventionalcommits",
-			},
-		],
-		[
-			"@semantic-release/changelog",
-			{
-				changelogTitle: "# Changelog",
-			},
-		],
-		[
-			"@semantic-release/exec",
-			{
-				prepareCmd: "poetry version ${nextRelease.version}",
-				publishCmd:
-					"poetry publish --build --username __token__ --password $PYPI_TOKEN --no-interaction -vvv",
-			},
-		],
-		[
-			"@semantic-release/github",
-			{
-				assets: [
-					{ path: "dist/*.tar.gz", label: "sdist" },
-					{ path: "dist/*.whl", label: "wheel" },
-				],
-				// failComment: false,
-			},
-		],
-		[
-			"@semantic-release/git",
-			{
-				assets: ["pyproject.toml", "*/__init__.py", CHANGELOG_FILE],
-			},
-		],
-	],
+  // https://semantic-release.gitbook.io/semantic-release/usage/workflow-configuration
+  branches: [
+    { name: "main" },
+    { name: "next" },
+    { name: "+([0-9])?(.{+([0-9]),x}).x" },
+    { name: "dev", prerelease: true },
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogTitle: "# Changelog",
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "poetry version ${nextRelease.version}",
+        publishCmd:
+          "poetry publish --build --username __token__ --password $PYPI_TOKEN --no-interaction -vvv",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "dist/*.tar.gz", label: "sdist" },
+          { path: "dist/*.whl", label: "wheel" },
+        ],
+        // failComment: false,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["pyproject.toml", "*/__init__.py", "CHANGELOG.md"],
+      },
+    ],
+  ],
 };
 
 module.exports = config;
